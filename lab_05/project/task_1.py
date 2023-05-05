7d4ff08d67a5b36615ed54d26af1a88cb6a35095
mshsvnv <mshsvnv@mail.ru>
2023-05-05 03:10:34 +0300

    add 5th lab

---
+++ lab_05/project/task_1.py
@@ -0,0 +1,14 @@
+import sympy as sym
+
+vars = ['x', 'y', 'z']
+funcs = ['x ** 2 + y ** 2 + z ** 2 - 1',
+         '2 * x ** 2 + y ** 2 - 4 * z',
+         '3 * x ** 2 - 4 * y + z ** 2']
+
+F = sym.Matrix(funcs)
+JacobianInv = sym.Matrix(funcs).jacobian(vars).det()
+
+x, y, z = sym.symbols('x y z')
+f = sym.lambdify([x, y, z], F)
+
+print(f(0, 0, 0))
